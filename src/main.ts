32c4b562c476a555ce54149fe2b0f994ccd9791b
Ajit <zlksnkwork@gmail.com>
2025-02-27 20:58:58 +0530

    remove wobble and add device pixel ratio

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import CanvasKitInit, { type Canvas, type Paint } from "canvaskit-wasm/full";
 import canvaskitwasm from "../node_modules/canvaskit-wasm/bin/full/canvaskit.wasm";
 import { SpringValue } from "@react-spring/core";
 
+const pd = window.devicePixelRatio;
+
 const canvas = document.getElementById("root") as HTMLCanvasElement | null;
 if (canvas === null) {
   throw new Error("check if the element with id `root` exists");
@@ -9,12 +11,14 @@ if (canvas === null) {
 if (canvas.parentElement === null) {
   throw new Error("check if the `root` element has a parent");
 }
+canvas.width = canvas.parentElement.clientWidth * pd;
+canvas.height = canvas.parentElement.clientHeight * pd;
 
 const resizeObserver = new ResizeObserver((entries) => {
   // This callback runs whenever the observed element changes size
   if (canvas === null || canvas.parentElement === null) return;
-  canvas.width = canvas.parentElement.clientWidth;
-  canvas.height = canvas.parentElement.clientHeight;
+  canvas.width = canvas.parentElement.clientWidth * pd;
+  canvas.height = canvas.parentElement.clientHeight * pd;
 });
 resizeObserver.observe(canvas.parentElement);
 
@@ -40,23 +44,40 @@ CanvasKitInit({
     config: { mass: 1, tension: 100, friction: 10 },
   });
 
+  let drawnOnce = false;
+
   function draw(canvas: Canvas) {
+    if (!drawnOnce) {
+      drawnOnce = true;
+      canvas.scale(pd, pd);
+    }
+
     if (surface === null) {
       throw new Error("surface is null within draw()");
     }
     const canvasWidth = surface.width();
     const canvasHeight = surface.width();
 
-    canvas.clear(CanvasKit.BLACK);
+    canvas.clear(CanvasKit.WHITE);
 
     newPaint();
     paint.setColor(CanvasKit.BLUE);
     paint.setStyle(CanvasKit.PaintStyle.Fill);
-    canvas.drawCircle(s.get(), 80, 40, paint);
+    // canvas.drawCircle(s.get(), 80, 40, paint);
+    canvas.drawCircle(100, 100, 30, paint);
+
+    newPaint();
+    paint.setColor(CanvasKit.RED);
+    paint.setStyle(CanvasKit.PaintStyle.Stroke);
+    const rect = CanvasKit.RRectXY(
+      CanvasKit.XYWHRect(200, 200, 200, 200),
+      15,
+      15
+    );
+    canvas.drawRRect(rect, paint);
 
     // change co-ordinate space of the whole canvas if needed
     // https://skia.org/docs/user/coordinates/#transforming-local-coordinate-space
-
     surface.requestAnimationFrame(draw);
   }
 
